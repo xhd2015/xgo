81e8dd1eff64e3af5816a6c000876d61c42bdabc
xhd2015 <xhd2015@gmail.com>
2024-04-01 20:04:05 +0800

    fix copyExpr with *syntax.ListExpr (#19)

--- cmd/xgo/version.go
+++ cmd/xgo/version.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 const VERSION = "1.0.11"
-const REVISION = "b1b21571259df7c1632d86cef35ba681727a0cde+1"
-const NUMBER = 143
+const REVISION = "43756010e13cabfae008c1de9d72f98b946b0a09+1"
+const NUMBER = 144
 
 func getRevision() string {
 	return fmt.Sprintf("%s %s BUILD_%d", VERSION, REVISION, NUMBER)
--- runtime/core/version.go
+++ runtime/core/version.go
@@ -7,8 +7,8 @@ import (
 )
 
 const VERSION = "1.0.11"
-const REVISION = "b1b21571259df7c1632d86cef35ba681727a0cde+1"
-const NUMBER = 143
+const REVISION = "43756010e13cabfae008c1de9d72f98b946b0a09+1"
+const NUMBER = 144
 
 // these fields will be filled by compiler
 const XGO_VERSION = ""
