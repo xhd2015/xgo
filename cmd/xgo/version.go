9e1fc983567b4d328ad769fa40d8be00678351cf
xhd2015 <xhd2015@gmail.com>
2024-04-02 12:10:07 +0800

    upgrade version to 1.0.12

--- cmd/xgo/version.go
+++ cmd/xgo/version.go
@@ -2,9 +2,9 @@ package main
 
 import "fmt"
 
-const VERSION = "1.0.11"
-const REVISION = "9144e01f0f4847402d24c04997c8077fbb3ae85e+1"
-const NUMBER = 146
+const VERSION = "1.0.12"
+const REVISION = "f3d7271450fef6b7575368a82d6fe254c894a97e+1"
+const NUMBER = 147
 
 func getRevision() string {
 	return fmt.Sprintf("%s %s BUILD_%d", VERSION, REVISION, NUMBER)
