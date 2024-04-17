f943b080bb465c7f56b9ebb01006ce4696a5c7b8
xhd2015 <xhd2015@gmail.com>
2024-04-17 18:55:22 +0800

    make trace auto fail next port

--- cmd/xgo/version.go
+++ cmd/xgo/version.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 const VERSION = "1.0.25"
-const REVISION = "ae8695c56e8c7f1976d409c7fba953041983c299+1"
-const NUMBER = 187
+const REVISION = "965c202f092256db5171d680d92030aa720cca4d+1"
+const NUMBER = 188
 
 func getRevision() string {
 	revSuffix := ""
