1e7392bbb5ba779fd9c79631dbbb31a6049657ac
xhd2015 <xhd2015@gmail.com>
2024-05-08 08:57:39 +0800

    silent prompt message when version same

--- cmd/xgo/version.go
+++ cmd/xgo/version.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 const VERSION = "1.0.29"
-const REVISION = "818c18ff3d125cf9734fe4d554655c922d645534+1"
-const NUMBER = 205
+const REVISION = "81a667d545fbd097e4403650a69af2c8477814fc+1"
+const NUMBER = 206
 
 func getRevision() string {
 	revSuffix := ""
