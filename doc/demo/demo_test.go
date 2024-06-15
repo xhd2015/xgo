1a16a856a24d88d7f84f61a39340a2b264e99b10
alpha-baby <fujianhao1997@qq.com>
2024-06-15 14:35:51 +0800

    demo: add more demo

--- doc/demo/demo_test.go
+++ doc/demo/demo_test.go
@@ -18,3 +18,73 @@ func TestFuncMock(t *testing.T) {
 		t.Fatalf("expect MyFunc() to be 'mock func', actual: %s", text)
 	}
 }
+
+type Student struct {
+	Name string
+	Age  int
+}
+
+func NewStudent() *Student {
+	return &Student{
+		Name: "xiaoming",
+		Age:  18,
+	}
+}
+
+func (s *Student) GetName() string {
+	return s.Name
+}
+
+func (s *Student) SetName(name string) {
+	s.Name = name
+}
+
+func (s *Student) getAge() int {
+	return s.Age
+}
+
+func TestStructPatch(t *testing.T) {
+	student := NewStudent()
+	mock.Patch(student.GetName, func() string {
+		return "zhangsan"
+	})
+
+	mock.Patch(student.getAge, func() int {
+		return 20
+	})
+
+	name := student.GetName()
+	if name != "zhangsan" {
+		t.Fatalf("expect GetName() to be 'zhangsan', actual: %s", name)
+	}
+
+	age := student.getAge()
+	if age != 20 {
+		t.Fatalf("expect getAge() to be '20', actual: %v", age)
+	}
+
+	mock.Patch(student.SetName, func(name string) {
+		return
+	})
+	student.SetName("lisi")
+
+	if student.Name != "xiaoming" {
+		t.Fatalf("expect Name to be 'xiaoming', actual: %s", student.Name)
+	}
+}
+
+type IStudent interface {
+	GetName() string
+}
+
+func TestInterfaceStructPatch(t *testing.T) {
+	var iStudent IStudent
+	iStudent = NewStudent()
+	mock.Patch(iStudent.GetName, func() string {
+		return "zhangsan"
+	})
+	name := iStudent.GetName()
+	if name != "zhangsan" {
+		t.Fatalf("expect GetName() to be 'zhangsan', actual: %s", name)
+	}
+}
--- doc/demo/go.mod
+++ doc/demo/go.mod
@@ -1,5 +1,9 @@
 module demo
 
-go 1.21.7
+go 1.18
 
-require github.com/xhd2015/xgo/runtime v1.0.7
+require github.com/xhd2015/xgo/runtime v1.0.40
+
+replace (
+	github.com/xhd2015/xgo/runtime => ../../runtime
+)
