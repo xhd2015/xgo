e6aa3a26a40d86be69c172b31d5b44ede02d849f
xhd2015 <xhd2015@gmail.com>
2025-03-30 21:14:38 +0800

    remove CallOld and fix Record for varadic functions

--- runtime/test/trace/record/record_test.go
+++ runtime/test/trace/record/record_test.go
@@ -28,13 +28,13 @@ func C() string {
 	return "C"
 }
 
-func TestRecord(t *testing.T) {
+func TestRecordCall(t *testing.T) {
 	var records []string
 	trace.RecordCall(A, func(res *string) {
 		records = append(records, fmt.Sprintf("A is called: %s", *res))
 	})
-	trace.RecordResult(B, func(res *string) {
-		records = append(records, fmt.Sprintf("B is called: %s", *res))
+	trace.RecordResult(B, func(res string) {
+		records = append(records, fmt.Sprintf("B is called: %s", res))
 	})
 	trace.RecordCall(C, func(res *string) {
 		records = append(records, fmt.Sprintf("C is called: %s", *res))
@@ -44,6 +44,21 @@ func TestRecord(t *testing.T) {
 		"C is called: \n" +
 		"B is called: B\n" +
 		"C is called: "
+	if diff := assert.Diff(expected, strings.Join(records, "\n")); diff != "" {
+		t.Error(diff)
+	}
+}
+
+func Varadic(msg string, args ...string) {
+}
+
+func TestRecordVaradic(t *testing.T) {
+	var records []string
+	trace.RecordCall(Varadic, func(msg *string, args *[]string) {
+		records = append(records, fmt.Sprintf("Varadic is called: %s, %v", *msg, *args))
+	})
+	Varadic("hello", "world", "foo", "bar")
+	expected := "Varadic is called: hello, [world foo bar]"
 	if diff := assert.Diff(expected, strings.Join(records, "\n")); diff != "" {
 		t.Error(diff)
 	}
